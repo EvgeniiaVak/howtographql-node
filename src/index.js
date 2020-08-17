13f94f9281ac5ae42bb0b447773d91e848645d2c
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2020-08-17 23:27:53 +0700

    adds subscription

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const {GraphQLServer} = require('graphql-yoga')
 const {PrismaClient} = require('@prisma/client')
+const {PubSub} = require('graphql-yoga')
 
+const Subscription = require('./resolvers/Subscription')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
 
 const prisma = new PrismaClient()
+const pubsub = new PubSub()
 
 const resolvers = {
+  Subscription,
   Query,
   Mutation,
   User,
   Link,
+  Vote,
 }
 
 const server = new GraphQLServer({
@@ -22,6 +28,7 @@ const server = new GraphQLServer({
     return {
       ...request,
       prisma,
+      pubsub,
     }
   },
 })
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -37,13 +37,17 @@ async function login(parent, args, context, info) {
 async function post(parent, args, context, info) {
   const userId = getUserId(context)
 
-  return await context.prisma.link.create({
+  const link = await context.prisma.link.create({
     data: {
       url: args.url,
       description: args.description,
       postedBy: { connect: { id: userId } },
     }
   })
+
+  context.pubsub.publish("NEW_LINK", link)
+
+  return link;
 }
 
 async function updateLink(parent, args, context) {
@@ -75,10 +79,39 @@ async function deleteLink(parent, args, context) {
   return deletedLink
 }
 
+
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context)
+
+  const vote = await context.prisma.vote.findOne({
+    where: {
+      linkId_userId: {
+        linkId: Number(args.linkId),
+        userId: userId
+      }
+    }
+  })
+
+  if (Boolean(vote)) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  const newVote = context.prisma.vote.create({
+    data: {
+      user: { connect: { id: userId } },
+      link: { connect: { id: Number(args.linkId) } },
+    }
+  })
+  context.pubsub.publish("NEW_VOTE", newVote)
+
+  return newVote
+}
+
 module.exports = {
   signup,
   login,
   post,
   updateLink,
-  deleteLink
+  deleteLink,
+  vote,
 }
